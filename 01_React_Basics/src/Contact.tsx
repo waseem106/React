d0f5354ae6baf707ed84bd2e7abef6489cc229e5
Waseem Aziz <Waseemaziz106@gmail.com>
2025-04-14 17:51:49 +0500

    first react basic project

---
+++ 01_React_Basics/src/Contact.tsx
@@ -0,0 +1,11 @@
+
+
+function Contact() {
+  return (
+    <div className="text-4xl text-center mt-[10%] hover:text-green-600" >
+      <p>I am contact page</p>
+    </div>
+  )
+}
+
+export default Contact
